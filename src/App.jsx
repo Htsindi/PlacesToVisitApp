2c73cda5b93031694622178253c1e8f28b65cc35
Martin <matefortechnology@gmail.com>
2024-06-23 16:45:55 +0200

    When to and not to use useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from "react";
 
-import Places from './components/Places.jsx';
-import { AVAILABLE_PLACES } from './data.js';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
-import {sortedPlacesByDistance} from './loc.js';
+import Places from "./components/Places.jsx";
+import { AVAILABLE_PLACES } from "./data.js";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import { sortPlacesByDistance } from "./loc.js";
+
+//Place outside so not to useEffect as it runs once when loading page
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces'))||[];
+const storedPlaces = storedIds.map((id)=>
+AVAILABLE_PLACES.find((place)=>place.id === id)
+);
 
 function App() {
-  const modal = useRef();
-  const selectedPlace = useRef();
+   const selectedPlace = useRef();
+   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
-  useEffect(()=>{
- //Becomes a loop if inside the app as it will run after the app functons thuats why use useEffect 
- navigator.geolocation.getCurrentPosition((position)=>{
-  const sortedPlaces = sortedPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude);
-  setAvailablePlaces(sortedPlaces);
-});
+  useEffect(() => {
+    //Becomes a loop if inside the app as it will run after the app functons thuats why use useEffect
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      setAvailablePlaces(sortedPlaces);
+    });
   }, []);
- 
+
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setModalIsOpen(true)
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setModalIsOpen(false)
   }
 
   function handleSelectPlace(id) {
@@ -38,18 +48,33 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    //convert string to json from browser
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    //store place on the browser as string side effect not needing useeffect bcoz its inside a fctn
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
+    setModalIsOpen(false);
+
+  //code for deleting items from local storage
+  const storeIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+  localStorage.setItem('selectedPlaces', JSON.stringify(storeIds.filter((id)=> id !== selectedPlace.current)));
+
   }
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal  open={modalIsOpen} onClose={handleStopRemovePlace} >
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
@@ -67,14 +92,14 @@ function App() {
       <main>
         <Places
           title="I'd like to visit ..."
-          fallbackText={'Select the places you would like to visit below.'}
+          fallbackText={"Select the places you would like to visit below."}
           places={pickedPlaces}
           onSelectPlace={handleStartRemovePlace}
         />
         <Places
           title="Available Places"
           places={availablePlaces}
-          fallbackText='Sorting Places By Distance ...'
+          fallbackText="Sorting Places By Distance ..."
           onSelectPlace={handleSelectPlace}
         />
       </main>
